4eb0a0a4a152f540a4c82d941a538a2c08b1a038
omaradam1 <99610975+omaradam1@users.noreply.github.com>
2023-07-11 20:59:11 +0300

    Update new.py

--- new.py
+++ new.py
@@ -1,1 +1,16 @@
 # HI Adam
+#make gif py python
+
+from PIL import Image, ImageDraw
+import glob
+
+frames = []
+imgs = glob.glob('adam1.jpeg')
+
+for i in imgs:
+    new_frame = Image.open(i)
+    frames.append(new_frame)
+
+
+frames[0].save(
+    'my.gif' , format='GIF' , append_images=frames[1:],save_all=True, duration=300, loop=0
